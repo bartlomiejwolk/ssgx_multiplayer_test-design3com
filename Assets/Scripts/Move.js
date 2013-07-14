401a56e1734a3eee0fc06bb42302bd07bcb15883
Bartłomiej Wołk <bartlomiejwolk@gmail.com>
2013-07-14 22:26:56 +0200

    - added colors - fixed camera

---
+++ Assets/Scripts/Move.js
@@ -0,0 +1,60 @@
+#pragma strict
+
+var speedShadow : GameObject;
+var forcePower : float = 8000;
+private var Charge : float;
+private var Energy : float = 0.6;
+
+private var MoveDirection : Vector3;
+private var prevTransformPosition : Vector3;
+
+private var horizAxis : float;
+private var vertAxis : float;
+
+private var shadowOn : boolean = false;
+
+function Update () {
+	horizAxis = Input.GetAxis("Horizontal");
+	vertAxis = Input.GetAxis("Vertical");
+	
+	if (Input.GetKeyDown("r")){
+		transform.rotation = Quaternion.identity;
+		transform.position = Vector3(2, 8, 9);
+		rigidbody.velocity = Vector3.zero;
+	}
+	
+	// Player movement control
+	if(networkView.isMine){
+		Rush();
+		
+		rigidbody.AddForce (Vector3(horizAxis * forcePower, -1000, vertAxis * forcePower) * Time.deltaTime);
+		
+		if (Energy <= 0){
+			if (transform.position != prevTransformPosition){
+				MoveDirection = (transform.position - prevTransformPosition).normalized;
+			}
+		}
+		Charge += Time.deltaTime;
+		if (Input.GetKeyDown("space") && Charge >= 1){
+			Energy = 0.2;
+		} prevTransformPosition = transform.position;
+	}
+	else { // Disable for online player
+		gameObject.tag = "OnlinePlayer";
+		enabled = false;
+	}
+}
+
+function Rush (){
+	if (Energy > 0){Charge = 0;
+		rigidbody.AddForce (MoveDirection * 80 * Time.deltaTime, ForceMode.Impulse);
+			while (!shadowOn && Energy > 0){
+				var cubeShadow : GameObject = Instantiate (speedShadow, transform.position, transform.rotation);
+				Destroy (cubeShadow, 0.24);
+				shadowOn = true;
+					yield WaitForSeconds (0.04);
+				shadowOn = false;
+			}
+		Energy -= Time.deltaTime;
+	}
+}
